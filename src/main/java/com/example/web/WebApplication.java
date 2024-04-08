89841dc95111778acfa89b0c93d027e3837560d9
hxr <2435222345@qq.com>
2024-04-08 11:23:28 +0800

    配置mybatis前的代码

--- src/main/java/com/example/web/WebApplication.java
+++ src/main/java/com/example/web/WebApplication.java
@@ -1,9 +1,11 @@
 package com.example.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.web.user.dao")
 public class WebApplication {
 
     public static void main(String[] args) {
